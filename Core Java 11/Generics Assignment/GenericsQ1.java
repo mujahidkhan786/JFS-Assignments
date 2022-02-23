565345ac2c3880367b22dc153424169458e624af
mujahidkhan786 <66386117+mujahidkhan786@users.noreply.github.com>
2022-02-24 01:30:14 +0530

    Add files via upload

---
+++ Core Java 11/Generics Assignment/GenericsQ1.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+public class GenericsQ1 
+{
+	public static void main(String[] args) 
+	{
+		        Set<String> Employee = new HashSet<>();
+		        {
+		        Employee.add("Id-RA1811003030213");
+		        Employee.add("Name-Mujahid Hussain Khan");
+		        Employee.add("Salary-27000");
+		        Employee.add("Department-Java Full Strack Developer");
+		        System.out.println(Employee);
+		        
+		    }
+		    	}
+		       
+		        	
+
+	}
+
