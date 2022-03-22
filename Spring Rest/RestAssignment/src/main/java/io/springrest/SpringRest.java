364503b1fdae13f471ab40acf6e7177575d5a285
mujahidkhan786 <66386117+mujahidkhan786@users.noreply.github.com>
2022-03-22 12:48:31 +0530

    My Commit
    this folder contains 4 ques of Spring Rest

---
+++ Spring Rest/RestAssignment/src/main/java/io/springrest/SpringRest.java
@@ -0,0 +1,12 @@
+package io.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRest.class,args);
+}
+}
