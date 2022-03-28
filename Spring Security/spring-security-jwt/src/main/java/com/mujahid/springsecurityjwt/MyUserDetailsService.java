87803e02db13e45fd3d69ddd25fc4ec3719fe415
Mujahid Hussain Khan <66386117+mujahidkhan786@users.noreply.github.com>
2022-03-29 04:12:22 +0530

    My commit

---
+++ Spring Security/spring-security-jwt/src/main/java/com/mujahid/springsecurityjwt/MyUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.mujahid.springsecurityjwt;
+
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return new User("mujahid", "Mujahid",
+                new ArrayList<>());
+    }
+}
+
